28c6bfec1e47533929a0591dad5bd1f9cf0cffd6
Dileep Kumar Maurya <dileep.maurya@dgliger.com>
2026-06-07 21:33:30 +0530

    update mune

--- reactjs/my-react-app/src/template/Header.js
+++ reactjs/my-react-app/src/template/Header.js
@@ -1,37 +1,63 @@
 import new_logo from "./../assets/img/new_logo.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 
 const Header = () => {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+     const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
     return (
-        <header class="site-header">
-            <div class="container nav-wrap">
-            <a href="#" class="brand">
-                <img src={new_logo} alt="AL-TRIUMPH Technology FZ-LLC logo" class="brand-logo"/>
-                <div class="brand-copy">
-                <strong>AL-TRIUMPH</strong>
-                <span>Technology</span>
-                </div>
-            </a>
+        <header className="site-header">
+        <div className="container nav-wrap">
+        <Link to="/" className="brand">
+          <img
+            src={new_logo}
+            alt="AL-TRIUMPH Technology FZ-LLC logo"
+            className="brand-logo"
+          />
+          <div className="brand-copy">
+            <strong>AL-TRIUMPH</strong>
+            <span>Technology</span>
+          </div>
+        </Link>
 
-            <button class="menu-toggle" aria-label="Open menu" id="menuToggle">
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
+        <button
+  className={`menu-toggle ${isMenuOpen ? "active" : ""}`}
+  aria-label="Toggle Menu"
+  onClick={() => setIsMenuOpen(!isMenuOpen)}
+>
+  <span></span>
+  <span></span>
+  <span></span>
+</button>
 
-            <nav class="nav-menu" id="navMenu">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/pricing">Pricing</Link>
-                <Link to="/team">Team</Link>
-                <Link to="/contact">Contact</Link>
-            </nav>
-
-            {/* <a href="#" class="btn btn-primary nav-cta">Get Started</a> */}
-            </div>
-  </header>
+        <nav className={`nav-menu ${isMenuOpen ? "active" : ""}`}>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
+          <Link to="/about" onClick={closeMenu}>
+            About
+          </Link>
+          <Link to="/projects" onClick={closeMenu}>
+            Projects
+          </Link>
+          <Link to="/pricing" onClick={closeMenu}>
+            Pricing
+          </Link>
+          <Link to="/team" onClick={closeMenu}>
+            Team
+          </Link>
+          <Link to="/contact" onClick={closeMenu}>
+            Contact
+          </Link>
+        </nav>
+      </div>
+    </header>
     )
 
 
